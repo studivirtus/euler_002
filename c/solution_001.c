61aa15f34a825e5d9943af0ed5bc5402a32d394c
studivirtus <studivirtus@gmail.com>
2018-03-31 16:06:52 -0500

    Added the filter for the even numbers and summed them up

--- c/solution_001.c
+++ c/solution_001.c
@@ -2,16 +2,29 @@
 
 int main()
 {
+	int x = 0;
 	int y = 1;
-	int x = 1;
+	int z = 0;
 	int sum = 0;
-	for (int c = 1; x < 100; c++)
+	int t = 0;
+	for (int c = 1; x < 4000000; c++)
 	{
-		int t = 0;
+
+		t = 0;
+		if (z == 3)
+		{
+			sum = sum + x;
+			z = 0;
+			//printf("%d", x);
+		}
+		z++;
+
 		t = x;
 		x = y + x;
 		y = t;
-		printf("%d\n", x);
+		//printf("\t%d", x);
+		//printf("\n");
 	}
+	printf("The sum of the even-valued terms of Fibonacci sequence whose values do not exceed four million:\n\t%d", sum);
 	return 0;
 }
