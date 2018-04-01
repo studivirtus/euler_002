d534f21676d74d5ac6095d875212bab14d9cd4e7
studivirtus <studivirtus@gmail.com>
2018-03-31 22:50:22 -0500

    C code mods
    Made the code less cluttered

--- c/solution_001.c
+++ c/solution_001.c
@@ -4,20 +4,17 @@ int main()
 {
 	int x = 0;
 	int y = 1;
-	int z = 0;
 	int sum = 0;
 	int t = 0;
-	for (int c = 1; x < 4000000; c++)
+	for (int c = 0; x < 4000000; c++)
 	{
-
 		t = 0;
-		if (z == 3)
+		if (c == 3)
 		{
 			sum = sum + x;
-			z = 0;
+			c = 0;
 			//printf("%d", x);
 		}
-		z++;
 
 		t = x;
 		x = y + x;
